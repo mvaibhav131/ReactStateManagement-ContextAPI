90fbbbe60d6ff2a03324cd1e573f70a279a38bf9
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2024-03-03 18:19:36 +0530

    adde theme privider to the app

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import { ThemeProvider } from './contextStore/themeContext';
 import Navbar from './layout/navbar';
 
 function App() {
   return (
+    <ThemeProvider>
     <div className="App">
     <Navbar/>
     </div>
+    </ThemeProvider>
   );
 }
 
